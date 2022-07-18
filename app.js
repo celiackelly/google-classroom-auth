cb83902a60be318b05580c1007c2ad3a879cc36b
Celia Kelly <celiackelly@gmail.com>
2022-07-18 17:38:08 -0400

    Remove unnecessary folder paths for public and views in server.js

--- app.js
+++ app.js
@@ -14,12 +14,11 @@ const app = express()
 connectDB()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 app.use(logger('dev'))
 app.use(cors())
 app.use(session({
