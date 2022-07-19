55df8d901e402061203e7b413f2cd9d6f98046e0
Celia Kelly <celiackelly@gmail.com>
2022-07-18 22:15:35 -0400

    Add Mongoose UserSchema

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const Mongoose = require('mongoose')
+
+const UserSchema = new Mongoose.Schema({
+    username: {
+        type: String, 
+        unique: true,
+        required: true,
+    }, 
+    password: {
+        type: String, 
+        minLength: 8, 
+        required: true
+    }, 
+    courses: {
+        type: Array,
+        default: [], 
+        required: true,
+    }
+})
+
+//use the schema to create a model 
+const User = Mongoose.model('user', UserSchema)
+
+module.exports = User
