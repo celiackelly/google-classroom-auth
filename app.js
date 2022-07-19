11d016f571ff0db744e9e31d321295b11318b5df
Celia Kelly <celiackelly@gmail.com>
2022-07-18 20:23:28 -0400

    Add basic navbar

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use(expressLayouts)
 app.use(logger('dev'))
 app.use(cors())
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
+	secret: process.env.SESSION_SECRET,
+	resave: true,
+	saveUninitialized: true,
 }))
 
 app.use('/', indexRouter)
@@ -39,18 +39,18 @@ app.use('/register', registerRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404))
+	next(createError(404))
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  	// set locals, only providing error in development
+	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+	// render the error page
+	res.status(err.status || 500)
+	res.render('error')
 });
 
 module.exports = app
